1bcbfa582c32d2160d350390bad6dc6850431aa1
Anais Mariana Rojas Rojas <arojas-r@c3r5s5.42madrid.com>
2024-04-19 17:01:15 +0200

    feat:modified atoi and calloc

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: arojas-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 11:10:39 by arojas-r          #+#    #+#             */
-/*   Updated: 2024/04/02 11:15:47 by arojas-r         ###   ########.fr       */
+/*   Updated: 2024/04/04 08:58:28 by arojas-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -26,3 +26,11 @@ int	ft_atoi(const char *str)
 		value = value * 10 + (*str++ - '0');
 	return (sign * value);
 }
+/*
+#include <stdio.h>
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		printf("%i\n", ft_atoi(argv[1]));
+	return (0);
+}*/
--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: arojas-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 11:19:12 by arojas-r          #+#    #+#             */
-/*   Updated: 2024/04/02 11:21:10 by arojas-r         ###   ########.fr       */
+/*   Updated: 2024/04/04 08:31:39 by arojas-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -21,3 +21,23 @@ void	*ft_calloc(size_t count, size_t size)
 	ft_bzero(ptr, (count * size));
 	return (ptr);
 }
+/*
+#include <stdio.h>
+int main(void)
+{
+    size_t count = 10;
+    char *result = ft_calloc(count, sizeof(char));
+
+	size_t len = 0;
+	while (len < count)
+	{
+		if (result[len] != 0)
+		{
+			printf("error");
+			return 0;
+		}
+		len++;
+	}
+	printf("right\n");
+	return 0;
+}*/
